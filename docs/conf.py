bbcce3499d520fc14fae8ca36f4eed5385e5c8bf
Martin Thoma <info@martin-thoma.de>
2023-03-11 13:39:11 +0100

    STY: Apply black
    Ran with:
        $ pip install black==23.1.0
        $ black .

--- docs/conf.py
+++ docs/conf.py
@@ -62,9 +62,9 @@ source_suffix = ".rst"
 master_doc = "index"
 
 # General information about the project.
-project = u"Camelot"
-copyright = u"2021, Camelot Developers"
-author = u"Vinayak Mehta"
+project = "Camelot"
+copyright = "2021, Camelot Developers"
+author = "Vinayak Mehta"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -283,7 +283,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, "Camelot.tex", u"Camelot Documentation", u"Vinayak Mehta", "manual"),
+    (master_doc, "Camelot.tex", "Camelot Documentation", "Vinayak Mehta", "manual"),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -323,7 +323,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, "Camelot", u"Camelot Documentation", [author], 1)]
+man_pages = [(master_doc, "Camelot", "Camelot Documentation", [author], 1)]
 
 # If true, show URL addresses after external links.
 #
@@ -339,7 +339,7 @@ texinfo_documents = [
     (
         master_doc,
         "Camelot",
-        u"Camelot Documentation",
+        "Camelot Documentation",
         author,
         "Camelot",
         "One line description of project.",
