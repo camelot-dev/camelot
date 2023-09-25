af738265b207569d4f3f9e23ab2056cb735881e6
geisserml <geisserml@gmail.com>
2023-09-25 20:25:43 +0200

    Capture possible error message
    In case the `try/except` captures something other than a classical
    ModuleNotFoundError, we want to know what happened (e.g. library load
    error)

--- camelot/backends/pdfium_backend.py
+++ camelot/backends/pdfium_backend.py
@@ -1,13 +1,16 @@
 try:
     import pypdfium2 as pdfium
-except Exception:
+except Exception as e:
     pdfium = None
+    pdfium_exc = e
+else:
+    pdfium_exc = None
 
 
 class PdfiumBackend:
     def convert(self, pdf_path, png_path, resolution=300):
         if not pdfium:
-            raise OSError("pypdfium2 is not installed.")
+            raise OSError(f"pypdfium2 is not available: {pdfium_exc!r}")
         doc = pdfium.PdfDocument(pdf_path)
         assert len(doc) == 1
         doc.init_forms()
