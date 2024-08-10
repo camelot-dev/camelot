1b67933cec5feda4d27b428dfd9603c10c13a42c
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2024-08-10 18:24:11 +0200

    [IMP] add Test for Python 3.12
    add py 3.12 to noxfile

--- noxfile.py
+++ noxfile.py
@@ -22,7 +22,7 @@ except ImportError:
     raise SystemExit(dedent(message)) from None
 
 package = "camelot"
-python_versions = ["3.10", "3.9", "3.8", "3.11"]
+python_versions = ["3.10", "3.9", "3.8", "3.11", "3.12"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
