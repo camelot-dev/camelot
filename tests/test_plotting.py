a3bccfb87897bc78b98f6d7857a69a5e28c8a1ef
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2024-09-21 17:08:06 +0200

    Disable plot tests on windows

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -14,7 +14,7 @@ def test_text_plot(testdir):
     tables = camelot.read_pdf(filename)
     return camelot.plot(tables[0], kind="text")
 
-
+@skip_on_windows
 @pytest.mark.mpl_image_compare(baseline_dir="files/baseline_plots", remove_text=True)
 def test_textedge_plot(testdir):
     filename = os.path.join(testdir, "tabula/12s0324.pdf")
@@ -37,7 +37,7 @@ def test_lattice_contour_plot_ghostscript(testdir):
     tables = camelot.read_pdf(filename, backend="ghostscript")
     return camelot.plot(tables[0], kind="contour")
 
-
+@skip_on_windows
 @pytest.mark.mpl_image_compare(baseline_dir="files/baseline_plots", remove_text=True)
 def test_stream_contour_plot(testdir):
     filename = os.path.join(testdir, "tabula/12s0324.pdf")
