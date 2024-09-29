2cadfa2f44895fbb05665fd3fdece3201d2bc2b3
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2024-09-29 15:12:46 +0200

    [IMP] Plotting tests

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -14,6 +14,7 @@ def test_text_plot(testdir):
     tables = camelot.read_pdf(filename)
     return camelot.plot(tables[0], kind="text")
 
+
 @skip_on_windows
 @pytest.mark.mpl_image_compare(baseline_dir="files/baseline_plots", remove_text=True)
 def test_textedge_plot(testdir):
@@ -37,12 +38,73 @@ def test_lattice_contour_plot_ghostscript(testdir):
     tables = camelot.read_pdf(filename, backend="ghostscript")
     return camelot.plot(tables[0], kind="contour")
 
+
 @skip_on_windows
 @pytest.mark.mpl_image_compare(baseline_dir="files/baseline_plots", remove_text=True)
 def test_stream_contour_plot(testdir):
     filename = os.path.join(testdir, "tabula/12s0324.pdf")
     tables = camelot.read_pdf(filename, flavor="stream")
     return camelot.plot(tables[0], kind="contour")
+
+
+@skip_on_windows
+@pytest.mark.mpl_image_compare(baseline_dir="files/baseline_plots", remove_text=True)
+def test_stream_grid_plot(testdir):
+    filename = os.path.join(testdir, "foo.pdf")
+    tables = camelot.read_pdf(filename, flavor="stream")
+    return camelot.plot(tables[0], kind="grid")
+
+
+@skip_on_windows
+@pytest.mark.mpl_image_compare(baseline_dir="files/baseline_plots", remove_text=True)
+def test_network_grid_plot(testdir):
+    filename = os.path.join(testdir, "foo.pdf")
+    tables = camelot.read_pdf(filename, flavor="network")
+    return camelot.plot(tables[0], kind="grid")
+
+
+@skip_on_windows
+@pytest.mark.mpl_image_compare(baseline_dir="files/baseline_plots", remove_text=True)
+def test_network_contour_plot(testdir):
+    filename = os.path.join(testdir, "tabula/12s0324.pdf")
+    tables = camelot.read_pdf(filename, flavor="network")
+    return camelot.plot(tables[0], kind="contour")
+
+
+@skip_on_windows
+@pytest.mark.mpl_image_compare(baseline_dir="files/baseline_plots", remove_text=True)
+def test_network_textedge_plot(testdir):
+    filename = os.path.join(testdir, "tabula/12s0324.pdf")
+    tables = camelot.read_pdf(filename, debug=True, flavor="network")
+    return camelot.plot(tables[0], kind="textedge")
+
+
+@skip_on_windows
+@pytest.mark.mpl_image_compare(baseline_dir="files/baseline_plots", remove_text=True)
+def test_network_table_regions_textedge_plot(testdir):
+    filename = os.path.join(testdir, "tabula/us-007.pdf")
+    tables = camelot.read_pdf(
+        filename, debug=True, flavor="network", table_regions=["320,505,573,330"]
+    )
+    return camelot.plot(tables[0], kind="textedge")
+
+
+@skip_on_windows
+@pytest.mark.mpl_image_compare(baseline_dir="files/baseline_plots", remove_text=True)
+def test_network_table_areas_text_plot(testdir):
+    filename = os.path.join(testdir, "tabula/us-007.pdf")
+    tables = camelot.read_pdf(
+        filename, debug=True, flavor="network", table_areas=["320,500,573,335"]
+    )
+    return camelot.plot(tables[0], kind="text")
+
+
+@skip_on_windows
+@pytest.mark.mpl_image_compare(baseline_dir="files/baseline_plots", remove_text=True)
+def test_network_table_search_plot(testdir):
+    filename = os.path.join(testdir, "tabula/us-007.pdf")
+    tables = camelot.read_pdf(filename, debug=True, flavor="network")
+    return camelot.plot(tables[0], kind="network_table_search")
 
 
 @skip_pdftopng
