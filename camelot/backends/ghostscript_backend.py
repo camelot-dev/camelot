441c443f27870d3c2599e4fab1c34a99e6556191
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2024-10-01 21:51:57 +0200

    simplify Fixup gs import

--- camelot/backends/ghostscript_backend.py
+++ camelot/backends/ghostscript_backend.py
@@ -1,37 +1,15 @@
-import ctypes
-import sys
-from ctypes.util import find_library
-
-
-def installed_posix():
-    library = find_library("gs")
-    return library is not None
-
-
-def installed_windows():
-    library = find_library(
-        "".join(("gsdll", str(ctypes.sizeof(ctypes.c_voidp) * 8), ".dll"))
-    )
-    return library is not None
 
 
 class GhostscriptBackend:
-    def installed(self):
-        if sys.platform in ["linux", "darwin"]:
-            return installed_posix()
-        elif sys.platform == "win32":
-            return installed_windows()
-        else:
-            return installed_posix()
 
     def convert(self, pdf_path, png_path, resolution=300):
-        if not self.installed():
+        try:
+            import ghostscript
+        except RuntimeError:
             raise OSError(
                 "Ghostscript is not installed. You can install it using the instructions"
                 " here: https://pypdf-table-extraction.readthedocs.io/en/latest/user/install-deps.html"
             )
-
-        import ghostscript
 
         gs_command = [
             "gs",
