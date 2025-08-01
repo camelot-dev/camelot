7aab8cdab3b250ed7fc1c9fcc08ac6ade9b37ffb
David Huggins-Daines <dhdaines@ecolingui.ca>
2025-08-01 08:38:52 -0600

    feat: replace pdfminer with paves.miner

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,32 +2,16 @@
 
 import os
 
-from pdfminer.converter import PDFPageAggregator
-from pdfminer.layout import LAParams
-from pdfminer.layout import LTTextBoxHorizontal
-from pdfminer.pdfinterp import PDFPageInterpreter
-from pdfminer.pdfinterp import PDFResourceManager
-from pdfminer.pdfpage import PDFPage
+import paves.miner as pm
+from paves.miner import LAParams
+from paves.miner import LTTextBoxHorizontal
 
 from camelot.utils import bbox_intersection_area
 
 
 def get_text_from_pdf(filename):
     """Method to extract text object from pdf."""
-    # https://stackoverflow.com/questions/22898145/how-to-extract-text-and-text-coordinates-from-a-pdf-file
-    # https://pdfminersix.readthedocs.io/en/latest/topic/converting_pdf_to_text.html
-    document = open(filename, "rb")
-    # Create resource manager
-    rsrcmgr = PDFResourceManager()
-    # Set parameters for analysis.
-    laparams = LAParams()
-    # Create a PDF page aggregator object.
-    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    for page in PDFPage.get_pages(document):
-        interpreter.process_page(page)
-        # receive the LTPage object for the page.
-        layout = device.get_result()
+    for layout in pm.extract(filename, LAParams()):
         for element in layout:
             if isinstance(element, LTTextBoxHorizontal):
                 return element
