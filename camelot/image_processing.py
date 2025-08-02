fix: apply rotation to threshold too

camelot/image_processing.py
@@ -25,7 +25,7 @@ def undo_rotation(pdf_image, rotation):
     return pdf_image
 
 
-def adaptive_threshold(imagename, process_background=False, blocksize=15, c=-2):
+def adaptive_threshold(imagename, process_background=False, blocksize=15, c=-2, rotation=""):
     """Thresholds an image using OpenCV's adaptiveThreshold.
 
     Parameters
@@ -46,6 +46,9 @@ def adaptive_threshold(imagename, process_background=False, blocksize=15, c=-2):
 
         For more information, refer `OpenCV's adaptiveThreshold
         <https://docs.opencv.org/2.4/modules/imgproc/doc/miscellaneous_transformations.html#adaptivethreshold>`_.
+    rotation: str, optional (default: "")
+        Either "" (no rotation), "clockwise", or "anticlockwise".  The
+        **inverse** of this rotation will be applied to the image.
 
     Returns
     -------
@@ -55,6 +58,7 @@ def adaptive_threshold(imagename, process_background=False, blocksize=15, c=-2):
         numpy.ndarray representing the thresholded image.
     """
     img = cv2.imread(imagename)
+    img = undo_rotation(img, rotation)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     if not process_background:
         gray = np.invert(gray)